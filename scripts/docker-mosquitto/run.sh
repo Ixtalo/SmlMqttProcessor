4c806f750b6ab0899f6e4104d4b26689eaa01e3c
Ixtalo <ixtalo@users.noreply.github.com>
2022-05-03 13:58:14 +0200

    docker scripts update

--- scripts/docker-mosquitto/run.sh
+++ scripts/docker-mosquitto/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -x
-docker run --rm -d -it -p 1883:1883 -p 9001:9001 -u 1000:1000 -v "$PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro" -v "$PWD/users.txt:/mosquitto/config/users.txt:ro" eclipse-mosquitto
+docker run --rm -it --name mqttserver -p 1883:1883 -p 9001:9001 -u 1000:1000 -v "$PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro" -v "$PWD/users.txt:/mosquitto/config/users.txt:ro" eclipse-mosquitto
+#sleep 3
+#docker logs --timestamps --follow mqttserver
