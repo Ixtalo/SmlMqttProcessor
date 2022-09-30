d0e741c7eeacf33e456b7ab3323324035ee0fc4e
Ixtalo <ixtalo@users.noreply.github.com>
2022-09-30 07:11:44 +0200

    subfolder test/testdata/

--- test/test_smltextmqttprocessor_libsml.py
+++ test/test_smltextmqttprocessor_libsml.py
@@ -53,11 +53,11 @@ def test_processing_loop_exampledatafiles_window1():
     Test all *.bin files for window_size=1
     """
     files2n = {
-        'EMH_eHZ-GW8E2A500AK2.bin': 16,
-        'EMH_eHZ-HW8E2AWL0EK2P.bin': 13,
-        'EMH_eHZ-IW8E2AWL0EK2P.bin': 12,
-        'ISKRA_MT175_eHZ.bin': 10,
-        'ISKRA_MT691_eHZ-MS2020.bin': 18
+        './testdata/EMH_eHZ-GW8E2A500AK2.bin': 16,
+        './testdata/EMH_eHZ-HW8E2AWL0EK2P.bin': 13,
+        './testdata/EMH_eHZ-IW8E2AWL0EK2P.bin': 12,
+        './testdata/ISKRA_MT175_eHZ.bin': 10,
+        './testdata/ISKRA_MT691_eHZ-MS2020.bin': 18
     }
     for filepath in iglob(os.path.join(__script_dir, '*.bin')):
         print(filepath)
@@ -72,11 +72,11 @@ def test_processing_loop_exampledatafiles_window2():
     Test all *.bin files for window_size=2
     """
     files2n = {
-        'EMH_eHZ-GW8E2A500AK2.bin': 8,
-        'EMH_eHZ-HW8E2AWL0EK2P.bin': 7,
-        'EMH_eHZ-IW8E2AWL0EK2P.bin': 6,
-        'ISKRA_MT175_eHZ.bin': 5,
-        'ISKRA_MT691_eHZ-MS2020.bin': 9
+        './testdata/EMH_eHZ-GW8E2A500AK2.bin': 8,
+        './testdata/EMH_eHZ-HW8E2AWL0EK2P.bin': 7,
+        './testdata/EMH_eHZ-IW8E2AWL0EK2P.bin': 6,
+        './testdata/ISKRA_MT175_eHZ.bin': 5,
+        './testdata/ISKRA_MT691_eHZ-MS2020.bin': 9
     }
     for filepath in iglob(os.path.join(__script_dir, '*.bin')):
         print(filepath)
@@ -91,11 +91,11 @@ def test_processing_loop_exampledatafiles_window15():
     Test all *.bin files for window_size=15
     """
     files2n = {
-        'EMH_eHZ-GW8E2A500AK2.bin': 2,
-        'EMH_eHZ-HW8E2AWL0EK2P.bin': 1,
-        'EMH_eHZ-IW8E2AWL0EK2P.bin': 1,
-        'ISKRA_MT175_eHZ.bin': 1,
-        'ISKRA_MT691_eHZ-MS2020.bin': 2
+        './testdata/EMH_eHZ-GW8E2A500AK2.bin': 2,
+        './testdata/EMH_eHZ-HW8E2AWL0EK2P.bin': 1,
+        './testdata/EMH_eHZ-IW8E2AWL0EK2P.bin': 1,
+        './testdata/ISKRA_MT175_eHZ.bin': 1,
+        './testdata/ISKRA_MT691_eHZ-MS2020.bin': 2
     }
     for filepath in iglob(os.path.join(__script_dir, '*.bin')):
         print(filepath)
