7b27f712b4dfc9dc1e2919c54029b70219492dd0
Ixtalo <ixtalo@users.noreply.github.com>
2024-11-21 17:16:49 +0100

    Local DEV scripts

--- scripts/README.md
+++ scripts/README.md
@@ -5,3 +5,9 @@ Some useful scripts for setup and development.
 - docker-mosquitto: Docker MQTT for local testing.
 - example-data-sender: simple Python script to simulate libsml's sml_server textual, decoded output.
 - systemd: for automatic starts, `smltextmqttprocessor.service`
+
+
+## Local Test Setup
+
+1. run `docker-mosquitto/run.sh` to start the MQTT server
+2. `DEBUG=1 poetry run python main.py --config config.dev.ini tests/testdata/ISKRA_MT175_eHZ.txt`
--- scripts/docker-mosquitto/run.sh
+++ scripts/docker-mosquitto/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -x
-docker run --rm -it --name mqttserver -p 1883:1883 -p 9001:9001 -u 1000:1000 -v "$PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro" -v "$PWD/users.txt:/mosquitto/config/users.txt:ro" eclipse-mosquitto
+# https://hub.docker.com/_/eclipse-mosquitto
+podman run --rm -it --name mqttserver -p 1883:1883 -p 9001:9001 -u 1000:1000 \
+  -v "$PWD/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro" \
+  -v "$PWD/users.txt:/mosquitto/config/users.txt:ro" \
+  docker.io/eclipse-mosquitto:2
 #sleep 3
 #docker logs --timestamps --follow mqttserver
