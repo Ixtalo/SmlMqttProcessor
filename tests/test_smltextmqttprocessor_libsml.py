293acf297c3898ebad147069c62fcb4c06d90f2f
Ixtalo <ixtalo@users.noreply.github.com>
2024-11-22 09:53:06 +0100

    fixes

--- tests/test_smltextmqttprocessor_libsml.py
+++ tests/test_smltextmqttprocessor_libsml.py
@@ -8,103 +8,122 @@ using pytest and libsml's sml_server_time.
 import logging
 import os
 import shlex
-from glob import iglob
+from pathlib import Path
 from subprocess import Popen, PIPE
+
 import pytest
+
 import smlmqttprocessor.smltextmqttprocessor as stmp
 
 # f-string does not work with Python 3.5
 # pylint: disable=consider-using-f-string
 
-# no docstring for tests
-# pylint: disable=missing-function-docstring
+# do not complain about missing docstring for tests
+# pylint: disable=missing-function-docstring,  missing-class-docstring
+# pylint: disable=line-too-long, too-few-public-methods
 # noqa: D102
 
-__script_dir = os.path.dirname(os.path.realpath(__file__))
+script_dir = Path(__file__).parent
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(name)-10s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-N = 0
+# counter how often processfile() has been called
+counter_processfile = 0
+
+@pytest.fixture
+def testdata_files():
+    # find all binary testdata files
+    return list(script_dir.joinpath('testdata').glob("*.bin"))
 
 
 def __processfile(filepath, window_size):
-    global N
+    global counter_processfile
 
     # reset counter
-    N = 0
+    counter_processfile = 0
 
     # aggregation callback
     def data_handler(_):
         # just count how often this has been called
-        global N
-        N += 1
+        global counter_processfile
+        counter_processfile += 1
 
     # call external binary and capture STDOUT
-    cmd = "%s %s" % (os.path.join(__script_dir, '../sml_server_time/sml_server_time'), filepath)
+    # the binary (sml_server_time) parses the binary files and produces text output
+    cmd = "%s %s" % (script_dir.joinpath('../sml_server_time/sml_server_time'), filepath)
     with Popen(shlex.split(cmd), stdout=PIPE) as proc:
         stmp.processing_loop(proc.stdout, window_size, data_handler, timeout=1)
 
-    return N
+    return counter_processfile
 
 
-def test_processing_loop_exampledatafiles_window1():
+def test_processing_loop_exampledatafiles_window1(testdata_files):
     """
     Test all *.bin files for window_size=1
     """
+    # predefined, empirically
     files2n = {
-        './testdata/EMH_eHZ-GW8E2A500AK2.bin': 16,
-        './testdata/EMH_eHZ-HW8E2AWL0EK2P.bin': 13,
-        './testdata/EMH_eHZ-IW8E2AWL0EK2P.bin': 12,
-        './testdata/ISKRA_MT175_eHZ.bin': 10,
-        './testdata/ISKRA_MT691_eHZ-MS2020.bin': 18
+        'EMH_eHZ-GW8E2A500AK2.bin': 16,
+        'EMH_eHZ-HW8E2AWL0EK2P.bin': 13,
+        'EMH_eHZ-IW8E2AWL0EK2P.bin': 12,
+        'ISKRA_MT175_eHZ.bin': 10,
+        'ISKRA_MT691_eHZ-MS2020.bin': 18
     }
-    for filepath in iglob(os.path.join(__script_dir, '*.bin')):
+    # make sure this test actually runs with all files
+    assert len(testdata_files) == 5
+    # action
+    for filepath in testdata_files:
         print(filepath)
-        filename = os.path.basename(filepath)
+        # action
         actual = __processfile(filepath, window_size=1)
-        expected = files2n[filename]
-        assert actual == expected, "Number mismatch for %s" % filename
+        # check
+        expected = files2n[filepath.name]
+        assert actual == expected, "Number mismatch for %s" % filepath.resolve()
 
 
-def test_processing_loop_exampledatafiles_window2():
+def test_processing_loop_exampledatafiles_window2(testdata_files):
     """
     Test all *.bin files for window_size=2
     """
     files2n = {
-        './testdata/EMH_eHZ-GW8E2A500AK2.bin': 8,
-        './testdata/EMH_eHZ-HW8E2AWL0EK2P.bin': 7,
-        './testdata/EMH_eHZ-IW8E2AWL0EK2P.bin': 6,
-        './testdata/ISKRA_MT175_eHZ.bin': 5,
-        './testdata/ISKRA_MT691_eHZ-MS2020.bin': 9
+        'EMH_eHZ-GW8E2A500AK2.bin': 8,
+        'EMH_eHZ-HW8E2AWL0EK2P.bin': 7,
+        'EMH_eHZ-IW8E2AWL0EK2P.bin': 6,
+        'ISKRA_MT175_eHZ.bin': 5,
+        'ISKRA_MT691_eHZ-MS2020.bin': 9
     }
-    for filepath in iglob(os.path.join(__script_dir, '*.bin')):
+    # make sure this test actually runs with all files
+    assert len(testdata_files) == 5
+    # action
+    for filepath in testdata_files:
         print(filepath)
-        filename = os.path.basename(filepath)
+        # action
         actual = __processfile(filepath, window_size=2)
-        expected = files2n[filename]
-        assert actual == expected, "Number mismatch for %s" % filename
+        # check
+        expected = files2n[filepath.name]
+        assert actual == expected, "Number mismatch for %s" % filepath.resolve()
 
 
-def test_processing_loop_exampledatafiles_window15():
+def test_processing_loop_exampledatafiles_window15(testdata_files):
     """
     Test all *.bin files for window_size=15
     """
     files2n = {
-        './testdata/EMH_eHZ-GW8E2A500AK2.bin': 2,
-        './testdata/EMH_eHZ-HW8E2AWL0EK2P.bin': 1,
-        './testdata/EMH_eHZ-IW8E2AWL0EK2P.bin': 1,
-        './testdata/ISKRA_MT175_eHZ.bin': 1,
-        './testdata/ISKRA_MT691_eHZ-MS2020.bin': 2
+        'EMH_eHZ-GW8E2A500AK2.bin': 2,
+        'EMH_eHZ-HW8E2AWL0EK2P.bin': 1,
+        'EMH_eHZ-IW8E2AWL0EK2P.bin': 1,
+        'ISKRA_MT175_eHZ.bin': 1,
+        'ISKRA_MT691_eHZ-MS2020.bin': 2
     }
-    for filepath in iglob(os.path.join(__script_dir, '*.bin')):
+    # make sure this test actually runs with all files
+    assert len(testdata_files) == 5
+    # action
+    for filepath in testdata_files:
         print(filepath)
-        filename = os.path.basename(filepath)
+        # action
         actual = __processfile(filepath, window_size=15)
-        expected = files2n[filename]
-        assert actual == expected, "Number mismatch for %s" % filename
-
-
-if __name__ == '__main__':
-    pytest.main()
+        # check
+        expected = files2n[filepath.name]
+        assert actual == expected, "Number mismatch for %s" % filepath.resolve()
