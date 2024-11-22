6680540cc1303726832318d974eee603c71bdc00
Ixtalo <ixtalo@users.noreply.github.com>
2024-11-22 10:06:49 +0100

    improvements, pytest.mark.skipif

--- tests/test_smltextmqttprocessor_libsml.py
+++ tests/test_smltextmqttprocessor_libsml.py
@@ -5,7 +5,6 @@ Run-time tests for smltextmqttprocessor.py,
 using pytest and libsml's sml_server_time.
 """
 
-import logging
 import shlex
 from pathlib import Path
 from subprocess import Popen, PIPE
@@ -23,44 +22,14 @@ import smlmqttprocessor.smltextmqttprocessor as stmp
 # noqa: D102
 
 script_dir = Path(__file__).parent
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)-8s %(name)-10s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-
-# counter how often processfile() has been called
-counter_processfile = 0
+sml_server_time_binary_filepath = script_dir.joinpath('../sml_server_time/sml_server_time')
+testdata_files = list(script_dir.joinpath('testdata').glob("*.bin"))
 
 
-@pytest.fixture
-def testdata_files():
-    # find all binary testdata files
-    return list(script_dir.joinpath('testdata').glob("*.bin"))
-
-
-def __processfile(filepath, window_size):
-    global counter_processfile
-
-    # reset counter
-    counter_processfile = 0
-
-    # aggregation callback
-    def data_handler(_):
-        # just count how often this has been called
-        global counter_processfile
-        counter_processfile += 1
-
-    # call external binary and capture STDOUT
-    # the binary (sml_server_time) parses the binary files and produces text output
-    cmd = "%s %s" % (script_dir.joinpath('../sml_server_time/sml_server_time'), filepath)
-    with Popen(shlex.split(cmd), stdout=PIPE) as proc:
-        stmp.processing_loop(proc.stdout, window_size, data_handler, timeout=1)
-
-    return counter_processfile
-
-
+@pytest.mark.skipif(not sml_server_time_binary_filepath.is_file(),
+                    reason="Binary sml_server_time must exist! (Build it first!)")
 @pytest.mark.parametrize(
-    "input_value,expected_numbers",
+    "input_value,expected_calls",
     [
         (1, {
             'EMH_eHZ-GW8E2A500AK2.bin': 16,
@@ -85,17 +54,38 @@ def __processfile(filepath, window_size):
         }),
     ]
 )
-def test_processing_loop(testdata_files, input_value, expected_numbers):
-    """
-    Test all *.bin files for window_size=15
-    """
+def test_processing_loop(input_value, expected_calls):
+    """Test all *.bin files for varying window sizes and check calls to processing."""
+
+    counter_processfile = 0
+
+    def processfile(filepath, window_size):
+        # global counter_processfile
+
+        # reset counter
+        counter_processfile = 0
+
+        # aggregation callback
+        def data_handler(_):
+            # just count how often this has been called
+            nonlocal counter_processfile
+            counter_processfile += 1
+
+        # call external binary and capture STDOUT
+        # the binary (sml_server_time) parses the binary files and produces text output
+        cmd = "%s %s" % (sml_server_time_binary_filepath, filepath)
+        with Popen(shlex.split(cmd), stdout=PIPE) as proc:
+            stmp.processing_loop(proc.stdout, window_size, data_handler, timeout=1)
+
+        return counter_processfile
+
     # make sure this test actually runs with all files
     assert len(testdata_files) == 5
     # action
     for filepath in testdata_files:
         print(filepath)
         # action
-        actual = __processfile(filepath, window_size=input_value)
+        actual = processfile(filepath, window_size=input_value)
         # check
-        expected = expected_numbers[filepath.name]
+        expected = expected_calls[filepath.name]
         assert actual == expected, "Number mismatch for %s" % filepath.resolve()
